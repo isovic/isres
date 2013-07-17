4b06a9f86c6021d123b89cdd286cb6210e023262
Ivan <ivan@ivan-T530.(none)>
2013-07-17 11:40:24 +0200

    Added memory units as a parameter to ./isres.py script.

--- examples/example2.sh
+++ examples/example2.sh
@@ -3,4 +3,4 @@
 loc=`dirname $0`
 
 # Calls a parallel-ismemtest.sh script that runs ismemtest in parallel. There are 4 processes, each allocating 102 MB of memory, and running for 3000 ms.
-$loc/../isres.py $loc/examples-output/example2 $loc/../ismemtest/parallel-ismemtest.sh
+$loc/../isres.py $loc/examples-output/example2 k $loc/../ismemtest/parallel-ismemtest.sh
